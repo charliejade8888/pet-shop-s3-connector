b7ca9a72f3d15b7402644c1cdc3f2ffa4e7fb193
Loki7281 <charliejade8888@gmail.com>
2025-08-19 14:22:22 +0100

    switched to lombok for properties

--- src/main/java/com/tyrell/replicant/config/MyConfigurationProperties.java
+++ src/main/java/com/tyrell/replicant/config/MyConfigurationProperties.java
@@ -1,9 +1,14 @@
 package com.tyrell.replicant.config;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
+// lombok used as Spring Boot 3.2+ has issues with records and @PropertySource
+@Getter
+@Setter
 @Configuration
 @ConfigurationProperties(prefix = "yaml")
 @PropertySource(value = "classpath:aws.yml", factory = YamlPropertySourceFactory.class)
@@ -15,44 +20,5 @@ public class MyConfigurationProperties {
   private String serviceEndpoint;
   private String signingRegion;
 
-  public String getSigningRegion() {
-    return signingRegion;
-  }
-
-  public void setSigningRegion(String signingRegion) {
-    this.signingRegion = signingRegion;
-  }
-
-  public String getServiceEndpoint() {
-    return serviceEndpoint;
-  }
-
-  public void setServiceEndpoint(String serviceEndpoint) {
-    this.serviceEndpoint = serviceEndpoint;
-  }
-
-  public String getAuthMethod() {
-    return authMethod;
-  }
-
-  public String getAccessKey() {
-    return accessKey;
-  }
-
-  public String getSecretKey() {
-    return secretKey;
-  }
-
-  public void setAuthMethod(String authMethod) {
-    this.authMethod = authMethod;
-  }
-
-  public void setAccessKey(String accessKey) {
-    this.accessKey = accessKey;
-  }
-
-  public void setSecretKey(String secretKey) {
-    this.secretKey = secretKey;
-  }
 
 }
